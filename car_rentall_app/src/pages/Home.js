8afdf56b79c46c9b0c4301a993de7e42762bb8cd
Jordan <jordan20022@gmail.com>
2024-09-18 20:30:57 +1200

    button example

--- car_rentall_app/src/pages/Home.js
+++ car_rentall_app/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import axios from 'axios';
+import Button from '@mui/material/Button';
 
 const Home = () => {
 
     const [data, setData] = React.useState("");
 
+    // fetch data from the server
     React.useEffect(() => {
         axios.get('http://localhost:80/INFO_project/Server/test.php')
         .then(function (response) {
@@ -21,6 +23,9 @@ const Home = () => {
         <div>
             <h1>Home</h1>
             <h2>{data}</h2>
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
         </div>
     );
 };
