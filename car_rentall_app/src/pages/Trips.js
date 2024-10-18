52ff815df70105475bbbd4d0f7cb76b8fc2876c7
Xanthe321321 <xwa146@uclive.ac.nz>
2024-10-18 16:37:07 +1300

    Update Trips.js
    im sick af

--- car_rentall_app/src/pages/Trips.js
+++ car_rentall_app/src/pages/Trips.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import AppBar from '../components/AppBar';
 import axios from 'axios';
 import TripsList from '../components/TripsList';
+import { Pagination, FormControl, InputLabel, Select, MenuItem, Checkbox, ListItemText, OutlinedInput, Slider } from '@mui/material';
 
 const defaultData = {
     "trips": [],
     "count": 0
 }
+const pageSize = 50; // need to change to dynamic
 // trips = [
 //         {
 //            'rego' 
@@ -24,10 +26,13 @@ const defaultData = {
 const Trips = () => {
 
     const [data, setData] = useState(defaultData);
+    const [page, setPage] = React.useState(1);
 
     React.useEffect(() => {
-        axios.get('http://localhost:80/INFO_project/Server/models/TripsModel.php', {
+        axios.get('http://localhost:80/INFO_project-main/Server/models/TripsModel.php', {
             params: {
+                startIndex: (page - 1) * pageSize,
+                num: pageSize,
                 //"rego": "Z"
             }
         })
@@ -38,7 +43,7 @@ const Trips = () => {
         .catch(function (error) {
             console.log(error);
         });
-    }, []);
+    }, [page]);
 
 
     return (
@@ -46,8 +51,19 @@ const Trips = () => {
             <AppBar/>
             <h1>Trips</h1>
             <h2>{data.count} trips found</h2>
+            <Pagination 
+                count={Math.ceil(data.count/pageSize)} 
+                color="primary" 
+                page={page}
+                onChange={(event, value) => setPage(value)}
+                style={{margin: "auto", justifyContent: "center", display: "flex"}}
+                />
+        
             <TripsList trips={data.trips}/>
+            
         </div>
+
+
     )
 }
 
