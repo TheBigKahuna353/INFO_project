4ada6ecc1ece91d320f402c47f716794f782b7ee
Xanthe321321 <xwa146@uclive.ac.nz>
2024-10-20 19:04:51 +1300

    Update MaintenanceListObj.js
    added image and capitalise cat

--- car_rentall_app/src/components/MaintenanceListObj.js
+++ car_rentall_app/src/components/MaintenanceListObj.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Paper, CardActionArea } from '@mui/material';
+import { Paper, Card, CardMedia, CardActionArea } from '@mui/material';
 
 const MaintenanceListObj = (main) => {
-
-
-
+    // Construct the image URL based on the category
+    const imageURL = "http://localhost/INFO_project-main/Server/models/ImageModel.php?type=" + main.category;
+    const capitalize = (str) => {
+        return str.toLowerCase().split('_').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    }
+    
     return (
         <div>
             <CardActionArea href={`/maintenance/${main.id}`}>
                 <Paper elevation={3}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            height="200" // Adjust height as needed
+                            sx={{ objectFit: "cover" }}
+                            image={imageURL}
+                            alt={`Maintenance for ${main.rego}`}
+                        />
+                    </Card>
                     <h2>{main.rego}</h2>
-                    <p>From: {main.location}</p>
-                    <p>To: {main.mileage}km</p>
+                    <p>Category: {capitalize(main.category)}</p> {/* Added category here */}
+                    <p>Location: {main.location}</p>
+                    <p>Mileage: {main.mileage} km</p>
                     <p>Start Date: {main.start_date}</p>
                     <p>End Date: {main.end_date}</p>
                 </Paper>
