ba74bda4b7ca493644cf3203994fc71856882330
Xanthe321321 <xwa146@uclive.ac.nz>
2024-10-20 20:41:07 +1300

    Update RelocationListObj.js

--- car_rentall_app/src/components/RelocationListObj.js
+++ car_rentall_app/src/components/RelocationListObj.js
@@ -1,37 +1,36 @@
 import React from 'react';
-import { Paper, Card, CardMedia, CardActionArea } from '@mui/material';
+import MaintenanceListObj from './MaintenanceListObj';
 
-const RelocationListObj = (relocation) => {
-    // Construct the image URL based on the category
-    const imageURL = "http://localhost/INFO_project-main/Server/models/ImageModel.php?type=" + relocation.category;
 
-    const capitalize = (str) => {
-        return str.toLowerCase().split('_').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+const MaintenanceList = ({maintenance}) => {
+
+    const card = {
+        padding: "10px",
+        margin: "20px",
+        display: "inline-grid",
+        width: "fit-content",
+        minWidth: "20%",
+        position: "relative",
     }
+
+    if (maintenance.count === 0) {
+        return <div>
+            <h1>Relocations</h1>
+            <p>Loading...</p>
+        </div>
+    }
+
     return (
-        <div>
-            <CardActionArea href={`/relocations/${relocation.id}`}>
-                <Paper elevation={3}>
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            height="200" // Adjust height as needed
-                            sx={{ objectFit: "cover" }}
-                            image={imageURL}
-                            alt={`Relocation for ${relocation.rego}`}
-                        />
-                    </Card>
-                    <h2>{relocation.rego}</h2>
-                    <p>Category: {capitalize(relocation.category)}</p> {/* Added category here */}
-                    <p>From: {relocation.origin}</p>
-                    <p>To: {relocation.destination}</p>
-                    <p>Distance: {relocation.distance} km</p>
-                    <p>Start Date: {relocation.start_date}</p>
-                    <p>End Date: {relocation.end_date}</p>
-                </Paper>
-            </CardActionArea>
+        <div style={{}}>
+            <div style={{}}>
+                {maintenance.map((main) => (
+                    <div key={main.rego+main.id} style={card}>
+                        <MaintenanceListObj {...main}/>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
 
-export default RelocationListObj;
+export default MaintenanceList;
